e0fb296e749d86e7549879b8e56aa6b0f560669c
Dima <uvarov3123@gmail.com>
2022-07-06 07:01:03 +0300

    add dark theme

--- source/js/script.js
+++ source/js/script.js
@@ -102,3 +102,68 @@ for (const burgerBtn of burgerBtns) {
 }
 //  \\\
 
+
+
+window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+
+    console.log('ree')
+    const htm1Block = document.documentElement;
+
+    const saveUserTheme = localStorage.getItem('user-theme');
+
+
+    let userTheme;
+    if (window.matchMedia) {
+        userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        console.log(userTheme)
+    }
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+        !saveUserTheme ? changeTheme() : null;
+    });
+
+    // 3mixna memu no Kniky
+    const themeButton = document.querySelector('.page__theme');
+    const resetButton = document.querySelector('.page_reset');
+    if (themeButton) {
+        themeButton.addEventListener("click", function (e) {
+            resetButton.classList.add('active');
+            changeTheme(true);
+        });
+    }
+    if (resetButton) {
+        resetButton.addEventListener("click", function (e) {
+            resetButton.classList.remove('active');
+            localStorage.setItem('user-theme', '');
+        });
+    }
+
+    function setThemeClass() {
+        if (saveUserTheme) {
+            htm1Block.classList.add(saveUserTheme)
+            resetButton.classList.add('active');
+        } else {
+            htm1Block.classList.add(userTheme);
+        }
+    }
+    setThemeClass();
+
+
+    function changeTheme(saveTheme = false) {
+        let currentTheme = htmlBlock.classList.contains('light') ? 'light' : 'dark';
+        let newTheme;
+
+        if (currentTheme === 'light') {
+            newTheme = 'dark';
+        } else if (currentTheme === 'dark') {
+            newTheme = 'light';
+        }
+
+
+        htm1Block.classList.remove(currentTheme);
+        htmlBlock.classList.add(newTheme);
+        saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
+    }
+
+}
